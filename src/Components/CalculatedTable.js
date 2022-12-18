63f96a97195e926948875262a36734672b3d9484
khpark <khpark@cognet9.com>
2022-12-18 21:31:30 +0900

    feature: calculated table, skeleton

--- src/Components/CalculatedTable.js
+++ src/Components/CalculatedTable.js
@@ -1,9 +1,45 @@
 import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
-class CalculatedTable extends React.Component {
-  render() {
-    return <h1>Hello, CalculatedTable</h1>;
-  }
+function CalculatedTable(props) {
+  const createData = (name, calories, fat, carbs, protein) => {
+    return { name, calories, fat, carbs, protein };
+  };
+
+  const rows = [
+    createData("skill levels(+skiller)", 159, 6.0, 24, 4.0),
+    createData("elemental mastery(+large charm)", 237, 9.0, 37, 4.3),
+    createData("min damage", 262, 16.0, 24, 6.0),
+    createData("max damage", 305, 3.7, 67, 4.3),
+  ];
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default CalculatedTable;
