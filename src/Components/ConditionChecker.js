b215b417fd1065888c43d3c10ed50a5bb1d8a8b3
khpark <khpark@cognet9.com>
2022-12-18 21:31:15 +0900

    feature: condition checker

--- src/Components/ConditionChecker.js
+++ src/Components/ConditionChecker.js
@@ -1,7 +1,62 @@
 import React from "react";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
 
 function ConditionChecker(props) {
-  return <h1>ConditionChecker</h1>;
+  const [baseSkillLevel, setBaseSkillLevel] = React.useState(0);
+  const [baseMastery, setBaseMastery] = React.useState(0);
+  const [baseSynergies, setSynergiesMastery] = React.useState(0);
+
+  const baseSkillChange = (event) => {
+    setBaseSkillLevel(event.target.value);
+  };
+  const baseMasteryChange = (event) => {
+    setBaseMastery(event.target.value);
+  };
+  const baseSynergiesChange = (event) => {
+    setSynergiesMastery(event.target.value);
+  };
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        id="filled-skill-level"
+        label="Skill Level"
+        type="number"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        value={baseSkillLevel}
+        onChange={baseSkillChange}
+      />
+      <TextField
+        id="filled-elemental-mastery"
+        label="Elemental Mastery"
+        type="number"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        value={baseMastery}
+        onChange={baseMasteryChange}
+      />
+      <TextField
+        id="filled-synergies"
+        label="Synergies"
+        type="number"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        value={baseSynergies}
+        onChange={baseSynergiesChange}
+      />
+    </Box>
+  );
 }
 
 export default ConditionChecker;
