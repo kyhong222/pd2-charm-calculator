17cb3910b886f73b5c5b470a7ef37cdb21319df0
khpark <khpark@cognet9.com>
2022-12-25 18:04:34 +0900

    feature: calculatedTable 개발

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import ConditionChecker from "./Components/ConditionChecker";
 import CalculatedTable from "./Components/CalculatedTable";
 import ClassAndSkillSelector from "./Components/ClassAndSkillSelector/ClassAndSkillSelector";
+import React from "react";
 
 function App() {
+  const [calSkillName, setCalSkillName] = React.useState("");
+  const [calSkillLevel, setCalSkillLevel] = React.useState(0);
+  const [calSynergies, setCalSynergies] = React.useState(0);
+  const [calMasteries, setCalMasteries] = React.useState(0);
+  // const [calMinDmg, setCalMinDmg] = React.useState(0);
+  // const [calMaxDmg, setCalMaxDmg] = React.useState(0);
+
   return (
     <div className="App">
-      <ClassAndSkillSelector />
-      <ConditionChecker />
-      <CalculatedTable />
+      <ClassAndSkillSelector skillName={setCalSkillName} />
+      <ConditionChecker
+        skillName={calSkillName}
+        skillLevel={setCalSkillLevel}
+        synergies={setCalSynergies}
+        masteries={setCalMasteries}
+      />
+      <CalculatedTable
+        skillName={calSkillName}
+        skillLevel={calSkillLevel}
+        synergies={calSynergies}
+        masteries={calMasteries}
+        // minDmg={calMinDmg}
+        // maxDmg={calMaxDmg}
+      />
     </div>
   );
 }
--- src/Components/CalculatedTable.js
+++ src/Components/CalculatedTable.js
@@ -1,26 +1,46 @@
 import React from "react";
+import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
 function CalculatedTable(props) {
-  const createData = (name, calories, fat, carbs, protein) => {
-    return { name, calories, fat, carbs, protein };
+  const createData = (name, values) => {
+    return { name, values };
   };
 
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
   const rows = [
-    createData("skill levels(+skiller)", 159, 6.0, 24, 4.0),
-    createData("elemental mastery(+large charm)", 237, 9.0, 37, 4.3),
-    createData("min damage", 262, 16.0, 24, 6.0),
-    createData("max damage", 305, 3.7, 67, 4.3),
+    createData("skill levels(+skiller)", [1, 2, 3, 4]),
+    createData("elemental mastery(+large charm)", [237, 9.0, 37, 4.3]),
+    createData("min damage", [262, 16.0, 24, 6.0]),
+    createData("max damage", [305, 3.7, 67, 4.3]),
   ];
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+            <StyledTableCell></StyledTableCell>
+            <StyledTableCell></StyledTableCell>
+            <StyledTableCell></StyledTableCell>
+            <StyledTableCell></StyledTableCell>
+          </TableRow>{" "}
+        </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
@@ -30,10 +50,11 @@ function CalculatedTable(props) {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              {row.values.map((value) => (
+                <TableCell component="th" scope="row">
+                  {value}
+                </TableCell>
+              ))}
             </TableRow>
           ))}
         </TableBody>
